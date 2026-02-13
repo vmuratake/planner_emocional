a4bc0d4d26f92c066b9bd6511748ce0dd894db8f
Vanessa Muratake <vanessamuratake@hotmail.com>
2026-02-13 00:42:12 -0300

    Feature(frontend): Base visual da página Nova Entrada com data e radios e nivel de energia

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,52 @@
-console.log("Página carregada com sucesso!");
+function formatarDataExtensaPTBR(data = new Date()) {
+    return new Intl.DateTimeFormat("pt-BR", {
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    }).format(data)
+}
+
+function renderizarDataTopo() {
+    const el = document.getElementById("dataAtual");
+    el.textContent = formatarDataExtensaPTBR(new Date());
+}
+
+
+function prepararFormularioMock() {
+    const form = document.getElementById("checkinForm");
+    const status = document.getElementById("status");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const selecionado = document.querySelector('input[name="nivel_energia"]:checked');
+        if (!selecionado) {
+            status.textContent = "Selecione um nível de energia para continuar.";
+            return
+        }
+
+        status.textContent = "Layout OK - Formulário pronto para integração futura.";
+        console.log("Nível de energia selecionado:", selecionado.value);
+    });
+
+
+        const btnLimpar = document.getElementById("btnLimpar");
+    
+    btnLimpar.addEventListener("click", () => {
+        //desmarca a opção nivel de energia selecionada
+        const selecionado = document.querySelector('input[name="nivel_energia"]:checked');
+        if (selecionado) {
+            selecionado.checked = false;
+        }   
+
+        //limpa o status e retorna a mensagem
+        status.textContent = "Formulário limpo. Selecione um nível de energia para continuar.";
+
+});
+}
+
+
+
+
+renderizarDataTopo();
+prepararFormularioMock();
