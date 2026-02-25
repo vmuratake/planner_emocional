989fb7e2be1df6516320a5a82a29415a6ff223f2
Vanessa Muratake <vanessamuratake@hotmail.com>
2026-02-25 02:22:02 -0300

    Feature(backend):Ajuste na porta para subir o site

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const { pool, testConnection } = require('./db')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 
 
 //rotas
@@ -155,5 +155,5 @@ app.post('/checkins', async (req, res) => {
 testConnection()
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`)
-})
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
